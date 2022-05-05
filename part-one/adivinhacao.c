55bc3dc77d08c0caa822119d14734da45a868dde
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-05-05 08:34:51 -0300

    first commit

---
+++ part-one/adivinhacao.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  printf("*******************************************\n");
+  printf("* Bem vindo ao nosso jogo de adivinihacao *\n");
+  printf("*******************************************\n");
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Aprendendo C
+
+## Parte 1
+
+- https://cursos.alura.com.br/course/introducao-a-programacao-com-c-parte-1
