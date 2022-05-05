1daa93b1fef6ede914adaf162beb3b1cc22d7611
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-05-05 08:49:50 -0300

    first module complete

--- part-one/adivinhacao.c
+++ part-one/adivinhacao.c
@@ -4,4 +4,12 @@ int main() {
   printf("*******************************************\n");
   printf("* Bem vindo ao nosso jogo de adivinihacao *\n");
   printf("*******************************************\n");
+
+  int numerosecreto = 42;
+  int chute;
+
+  printf("\nqual é o seu chute? ");
+  scanf("%d", &chute);
+
+  printf("\nseu chute foi %d\n", chute);
 }
